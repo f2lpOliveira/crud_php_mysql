935b967eb790fe7aeac0fac07ca69df5d6c41f1c
f2lpOliveira <contato@fellipefreire.com.br>
2023-12-16 19:55:32 -0300

    form cadastrar veículos.

---
+++ cadastrar.php
@@ -0,0 +1,10 @@
+<h1>Cadastrar Veículos</h1>
+<form method="POST" action="cadastrar_action.php">
+	<label>
+		Marca: <input type="text" name="marca"/>
+	</label>
+	<label>
+		Modelo: <input type="text" name="modelo"/>
+	</label>
+	<input type="submit" value="Salvar"/>
+</form>
